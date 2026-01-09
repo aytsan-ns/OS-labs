756e6a6c56d294befc0753ffe3c7c547f8ecb811
aytsan-ns <149725956+aytsan-ns@users.noreply.github.com>
2026-01-09 15:20:48 +0300

    Update conn_shm.hpp

--- lab2/conn_shm.hpp
+++ lab2/conn_shm.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstddef>
+#include <semaphore.h>
 
 class Conn {
 public:
@@ -13,8 +14,15 @@ public:
     bool Write(const void* buf, size_t count);
 
 private:
+    static int s_numKids;
+
     int m_kidId;
     bool m_isHost;
     int m_fd;
     void* m_mem;
+
+    sem_t* m_reqEmpty;
+    sem_t* m_reqFull;
+    sem_t* m_respEmpty;
+    sem_t* m_respFull;
 };
